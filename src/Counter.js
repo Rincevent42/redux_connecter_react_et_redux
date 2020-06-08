0ebd8689b6eb6e8a9d95d6b5249f734833e1b2c4
Rincevent42 <porvooc@gmail.com>
2020-06-08 18:25:18 +0200

    quelques améliorations

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const remove10 = { type: 'DEC10' };
+const remove1 = { type: 'DEC1' };
+const reset = { type: 'RESET' };
+const add1 = { type: 'INC1' };
+const add10 = { type: 'INC10' };
+
 class Counter extends Component {
   constructor (props) {
     super(props);
@@ -14,11 +20,11 @@ class Counter extends Component {
     return (
       <div>
         <p>{counter}</p>
-        <button onClick={() => dispatch({ type: 'DEC10' })}>-10</button>
-        <button onClick={() => dispatch({ type: 'DEC1' })}>-</button>
-        <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
-        <button onClick={() => dispatch({ type: 'INC1' })}>+</button>
-        <button onClick={() => dispatch({ type: 'INC10' })}>+10</button>
+        <button onClick={() => dispatch(remove10)}>-10</button>
+        <button onClick={() => dispatch(remove1)}>-</button>
+        <button onClick={() => dispatch(reset)}>Reset</button>
+        <button onClick={() => dispatch(add1)}>+</button>
+        <button onClick={() => dispatch(add10)}>+10</button>
       </div>
     );
   }
